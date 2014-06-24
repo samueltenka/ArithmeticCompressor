b407163264f0ee75d7bf2de84a506aaba9fb443d
samueltenka <iguavadon@gmail.com>
2014-06-24 02:42:02 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,65 +2,83 @@
 //
 
 #include "stdafx.h"
-//#include "TextFile.h"
 #include "Huffman.h"
+#include <string>
 #include <iostream>
 #include <fstream>
 using namespace std;
 
+
 #define PAUSE {char l; cin >> l;}
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	HuffmanCoder H;
-
-	//string lorem = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-				   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-	//string lorem = "Lorem ipsum dolor sit amet, \
-				   consectetur adipiscing elit. \
-				   Videamus animi partes, \
-				   quarum est conspectus illustrior; \
-				   Quae cum essent dicta, \
-				   finem fecimus et ambulandi et disputandi. \
-				   Ad corpus diceres pertinere, \
-				   sed ea, quae dixi, ad corpusne refers? \
-				   Duo Reges: constructio interrete. \
-				   Expectoque quid ad id, \
-				   quod quaerebam, respondeas. \
-				   Nunc ita separantur, ut disiuncta sint, \
-				   quo nihil potest esse perversius. \
-				   At miser, si in flagitiosa et \
-				   vitiosa vita afflueret voluptatibus.";
+	// load a very large file
 	string lorem;
-	ifstream myfile("C:\\Users\\Sam\\Desktop\\Compressor\\Genesis.txt");
-	for(int i = 0; myfile && i<16000; i++)
+	ifstream myfile("C:\\Users\\Sam\\Desktop\\Compressor\\GenesisAsciified.txt");
+	while(myfile)
 	{
 		lorem += myfile.get();
 	}
 	myfile.close();
-	cout << "read" << endl;
+	//string lorem = "why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there \
+				   why hello there why hello there why hello there why hello there why hello there";
 
-	string coded = H.encode(lorem);
-	cout << lorem.length()*8 << "->" << coded.length() << endl;
+	// compress it
+	string shorter;
+	Huffman H;
+	for(int n = 0; n < lorem.length(); n++)
+	{
+		if(n%1000 == 0)
+			PRINT(n)
+		shorter += H.eat(lorem[n]);
+	}
+	PRINT("YEAH!")
+	//PRINTL("shorter", shorter)
+	PRINTL("lorem length", lorem.length()*8);
+	PRINTL("shorter length", shorter.length());
+
+	// write
+
 
 	PAUSE
 	return 0;
 }
 
+
+
+
+
+
+
+
+
+
+/*	ifstream gen_in("C:\\Users\\Sam\\Desktop\\Compressor\\Genesis.txt");
+	ofstream gen_out("C:\\Users\\Sam\\Desktop\\Compressor\\GenesisAsciified.txt");
+	for(int i = 0; gen_in; i++)
+	{
+		gen_out.put(gen_in.get());
+	}
+	gen_out.close();
+	gen_in.close();
+*/
