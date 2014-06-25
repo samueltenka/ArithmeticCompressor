da8d1f299c48c596ac75004dbbcbb459e0c55365
samueltenka <iguavadon@gmail.com>
2014-06-25 01:43:18 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,40 +22,34 @@ int _tmain(int argc, _TCHAR* argv[])
 		lorem += myfile.get();
 	}
 	myfile.close();
-	//string lorem = "why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there \
-				   why hello there why hello there why hello there why hello there why hello there";
+
+	//string lorem = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, \
+				   totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. \
+				   Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, \
+				   sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. \
+				   Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, \
+				   sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. \
+				   Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? \
+				   Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, \
+				   vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
 
 	// compress it
 	string shorter;
 	Huffman H;
+	PRINTL("lorem ~#pages", lorem.length()/(6*500));
 	for(int n = 0; n < lorem.length(); n++)
 	{
-		if(n%1000 == 0)
-			PRINT(n)
+		//if(n>205634) //n%205635 == 0)
+		//{
+		//	PRINT(n)
+		//	H.X = true;
+		//}
 		shorter += H.eat(lorem[n]);
 	}
 	PRINT("YEAH!")
 	//PRINTL("shorter", shorter)
-	PRINTL("lorem length", lorem.length()*8);
-	PRINTL("shorter length", shorter.length());
+	PRINTL("lorem bits", lorem.length()*8);
+	PRINTL("shorter bits", shorter.length());
 
 	// write
 
@@ -63,13 +57,6 @@ int _tmain(int argc, _TCHAR* argv[])
 	PAUSE
 	return 0;
 }
-
-
-
-
-
-
-
 
 
 
